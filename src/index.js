247f4123a6147b0eecc1ece215677d00348fd2fa
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-07-25 04:13:24 +0500

    Delete unnecessary code that was created to test webpack setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
 import './style.css';
-import refresh from './assets/refresh.svg';
-
-const user = 'Zilola Nazarovas';
-document.querySelector('h1').textContent = user;
-document.getElementById('refresh-icon').src = refresh;
