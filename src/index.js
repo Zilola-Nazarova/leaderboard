17dfbe736fecf761933c1cbf5ba98d09e18760e7
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-07-26 06:01:10 +0500

    Add event listneres to buttons when clicked, to window when loaded and to inputs when key is released

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import './style.css';
+import addScore from './modules/add-score.js';
+import refreshList from './modules/refresh-list.js';
+import showError from './modules/show-error.js';
+import { takeFromLocal, setToLocal } from './modules/input-preserve.js';
+
+const refreshBtn = document.getElementById('refresh-btn');
+const submitBtn = document.getElementById('submit-btn');
+
+// Render list when page is loaded, prefill unsubmitted input
+window.addEventListener('load', () => {
+  refreshList();
+  if (localStorage.getItem('currentInput')) {
+    takeFromLocal();
+  } else {
+    setToLocal();
+  }
+});
+
+refreshBtn.addEventListener('click', () => {
+  refreshList();
+});
+
+submitBtn.addEventListener('click', async (event) => {
+  event.preventDefault();
+  const name = document.getElementById('user-name');
+  const score = document.getElementById('user-score');
+
+  if (name.value && score.value !== '') {
+    await addScore(name.value, score.value);
+  } else if (name.value === '') {
+    showError('name', 'Please, enter your name');
+  } else if (score.value === '') {
+    showError('score', 'Please, enter your score');
+  }
+});
+
+// Preserve input in localStorage for user comfort
+document.getElementById('user-name').onkeyup = setToLocal;
+document.getElementById('user-score').onkeyup = setToLocal;
